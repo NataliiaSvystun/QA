cdecf70f2488a34d97c2f9554878098920369100
Nataliia Svystun <nat20088@ukr.net>
2017-12-19 15:10:22 +0200

    created project, add Unity

---
+++ taskQuestions/taskQuestions/Models/Questions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace taskQuestions.Models
+{
+    public class Questions
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ taskQuestions/taskQuestions/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(taskQuestions.Startup))]
+namespace taskQuestions
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
