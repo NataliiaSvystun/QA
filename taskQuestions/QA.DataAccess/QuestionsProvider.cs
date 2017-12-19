a1c0a25655722e09372b91db38d4b781107e3721
Nataliia Svystun <nat20088@ukr.net>
2017-12-19 15:15:27 +0200

    created project, add unity

---
+++ taskQuestions/QA.DataAccess/QuestionsProvider.cs
@@ -0,0 +1,13 @@
+﻿using QA.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QA.DataAccess
+{
+    public class QuestionsProvider: IQuestionsProvider
+    {
+    }
+}
